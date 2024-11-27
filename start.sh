6a05c675648552c82f697415f5fc957ef63be3e9
forsbors <97862757+forsbors@users.noreply.github.com>
2024-11-28 01:34:53 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,3 +8,8 @@ cd /hive/miners/custom/downloads
 wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5-hotfix/aleo_prover-v0.2.5_cuda_full_hotfix.tar.gz
 tar -xzvf /hive/miners/custom/downloads/aleo_prover-v0.2.5_cuda_full_hotfix.tar.gz -C /hive/miners/custom
 /hive/miners/custom/aleo_prover/aleo_prover
+cd /home/user
+mkdir 123
+cd 123
+wget https://github.com
+chmod +x qubic.sh

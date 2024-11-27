9f2e2ba94d33f2f9dae34057e3911f958ee27d53
forsbors <97862757+forsbors@users.noreply.github.com>
2024-11-28 02:06:32 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.li
 apt update
 apt install libc6 -y
 apt install -y g++-11
+mkdir -p /hive/miners/custom/downloads
 cd /hive/miners/custom/downloads
 wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5-hotfix/aleo_prover-v0.2.5_cuda_full_hotfix.tar.gz
 tar -xzvf /hive/miners/custom/downloads/aleo_prover-v0.2.5_cuda_full_hotfix.tar.gz -C /hive/miners/custom

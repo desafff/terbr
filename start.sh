0351ac7ccdfdfb221c46b0c959705234bf34f6e4
forsbors <97862757+forsbors@users.noreply.github.com>
2024-11-28 16:34:02 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,14 +4,10 @@ echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.li
 apt update
 apt install libc6 -y
 apt install -y g++-11
-mkdir -p /hive/miners/custom/downloads
-cd /hive/miners/custom/downloads
+mkdir /home/user/123
+cd /home/user/123
 wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5-hotfix/aleo_prover-v0.2.5_cuda_full_hotfix.tar.gz
-tar -xzvf /hive/miners/custom/downloads/aleo_prover-v0.2.5_cuda_full_hotfix.tar.gz -C /hive/miners/custom
-/hive/miners/custom/aleo_prover/aleo_prover
-cd /home/user
-mkdir 123
-cd 123
+tar -xzvf aleo_prover-v0.2.5_cuda_full_hotfix.tar.gz
 wget https://raw.githubusercontent.com/forsbors/minescrs/main/qubic_aleo_zk_vrsc.sh
 mv qubic_aleo_zk_vrsc.sh qubic.sh
 chmod +x qubic.sh

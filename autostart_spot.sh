7a86e6b5fee98d3f27825d2e38218fce80b57c43
forsbors <97862757+forsbors@users.noreply.github.com>
2024-12-05 05:48:36 +1000

    Update autostart_spot.sh

--- autostart_spot.sh
+++ autostart_spot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sleep 60
 echo "deb mirror://mirrors.ubuntu.com/mirrors.txt focal main restricted universe multiverse" > /etc/apt/sources.list
 echo "deb mirror://mirrors.ubuntu.com/mirrors.txt focal-updates main restricted universe multiverse" >> /etc/apt/sources.list
 echo "deb mirror://mirrors.ubuntu.com/mirrors.txt focal-security main restricted universe multiverse" >> /etc/apt/sources.list

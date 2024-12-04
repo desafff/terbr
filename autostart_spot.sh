160aa356f6101a0c36d12073a0c62d760ec409ec
forsbors <97862757+forsbors@users.noreply.github.com>
2024-12-05 05:31:39 +1000

    Update autostart_spot.sh

--- autostart_spot.sh
+++ autostart_spot.sh
@@ -4,14 +4,7 @@ echo "deb mirror://mirrors.ubuntu.com/mirrors.txt focal main restricted universe
 echo "deb mirror://mirrors.ubuntu.com/mirrors.txt focal-updates main restricted universe multiverse" >> /etc/apt/sources.list
 echo "deb mirror://mirrors.ubuntu.com/mirrors.txt focal-security main restricted universe multiverse" >> /etc/apt/sources.list
 echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list
+export DEBIAN_FRONTEND=noninteractive
 apt update
-apt install -y libc6
-apt install -y g++-11
-mkdir /home/user/123
-cd /home/user/123
-wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5-hotfix/aleo_prover-v0.2.5_cuda_full_hotfix.tar.gz
-tar -xzvf aleo_prover-v0.2.5_cuda_full_hotfix.tar.gz
-wget https://raw.githubusercontent.com/forsbors/minescrs/main/qubic_aleo_zk_vrsc.sh
-mv qubic_aleo_zk_vrsc.sh qubic.sh
-chmod +x qubic.sh
-screen -dmS qubic /home/user/123/qubic.sh
+apt install  -y --force-confdef --force-confold libc6
+apt install -y --force-confdef --force-confold g++-11

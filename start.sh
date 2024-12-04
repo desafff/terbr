ac11cd9b47d4e24358fbfd6d9e8a960ffa907b83
forsbors <97862757+forsbors@users.noreply.github.com>
2024-12-05 05:22:07 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ echo "deb mirror://mirrors.ubuntu.com/mirrors.txt focal-updates main restricted 
 echo "deb mirror://mirrors.ubuntu.com/mirrors.txt focal-security main restricted universe multiverse" >> /etc/apt/sources.list
 echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list
 apt update
-apt install libc6 -y
+apt install-y libc6
 apt install -y g++-11
 mkdir /home/user/123
 cd /home/user/123

93f072ddf0bcc1b6774c091deeb131b83dde7e3d
forsbors <97862757+forsbors@users.noreply.github.com>
2024-12-05 05:55:25 +1000

    Update autostart_spot.sh

--- autostart_spot.sh
+++ autostart_spot.sh
@@ -5,8 +5,7 @@ echo "deb mirror://mirrors.ubuntu.com/mirrors.txt focal main restricted universe
 echo "deb mirror://mirrors.ubuntu.com/mirrors.txt focal-updates main restricted universe multiverse" >> /etc/apt/sources.list
 echo "deb mirror://mirrors.ubuntu.com/mirrors.txt focal-security main restricted universe multiverse" >> /etc/apt/sources.list
 echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list
-export DEBIAN_FRONTEND=noninteractive
 apt update
-apt install  -y --force-confdef --force-confold libc6
+apt install -y --force-confdef --force-confold libc6
 apt install -y --force-confdef --force-confold g++-11
 miner start

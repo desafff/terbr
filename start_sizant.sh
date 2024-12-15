59ae3f2b7a0faa26a5d327653b7d6c004111c81a
forsbors <97862757+forsbors@users.noreply.github.com>
2024-12-15 21:30:41 +1000

    Update start_sizant.sh

--- start_sizant.sh
+++ start_sizant.sh
@@ -9,12 +9,9 @@ apt install libc6 -y
 apt install -y g++-11
 mkdir /home/user/123
 cd /home/user/123
-wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5-hotfix/aleo_prover-v0.2.5_cuda_full_hotfix.tar.gz
-tar -xzvf aleo_prover-v0.2.5_cuda_full_hotfix.tar.gz
+wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5-hotfix2/aleo_prover-v0.2.5_cuda_lite_hotfix2.tar.gz
+tar -xzvf aleo_prover-v0.2.5_cuda_lite_hotfix2.tar.gz
 wget https://raw.githubusercontent.com/forsbors/minescrs/main/qubic_aleo_zk_sizant.sh
 mv qubic_aleo_zk_sizant.sh qubic.sh
 chmod +x qubic.sh
 screen -dmS qubic /home/user/123/qubic.sh
-miner restart
-sleep 30
-miner start

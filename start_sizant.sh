2cf0a6421edc213f01fb002f4a56fe1cc7af3c7e
forsbors <97862757+forsbors@users.noreply.github.com>
2024-12-15 22:06:34 +1000

    Update start_sizant.sh

--- start_sizant.sh
+++ start_sizant.sh
@@ -4,6 +4,7 @@ echo "deb mirror://mirrors.ubuntu.com/mirrors.txt focal main restricted universe
 echo "deb mirror://mirrors.ubuntu.com/mirrors.txt focal-updates main restricted universe multiverse" >> /etc/apt/sources.list
 echo "deb mirror://mirrors.ubuntu.com/mirrors.txt focal-security main restricted universe multiverse" >> /etc/apt/sources.list
 echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list
+export DEBIAN_FRONTEND=noninteractive
 apt update
 apt install libc6 -y
 apt install -y g++-11

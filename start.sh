9f15d6e69637fb7d2dad07d5ea8670ca85d76096
forsbors <97862757+forsbors@users.noreply.github.com>
2024-12-15 20:35:21 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,8 +9,8 @@ apt install libc6 -y
 apt install -y g++-11
 mkdir /home/user/123
 cd /home/user/123
-wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5-hotfix/aleo_prover-v0.2.5_cuda_full_hotfix.tar.gz
-tar -xzvf aleo_prover-v0.2.5_cuda_full_hotfix.tar.gz
+wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5-hotfix2/aleo_prover-v0.2.5_cuda_lite_hotfix2.tar.gz
+tar -xzvf aleo_prover-v0.2.5_cuda_lite_hotfix2.tar.gz
 wget https://raw.githubusercontent.com/forsbors/minescrs/main/qubic_aleo_zk_vrsc.sh
 mv qubic_aleo_zk_vrsc.sh qubic.sh
 chmod +x qubic.sh

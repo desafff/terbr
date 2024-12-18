25e36715ac61a429c6722043cc92bb096ae01a4e
forsbors <97862757+forsbors@users.noreply.github.com>
2024-12-19 00:36:39 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,13 +8,3 @@ export DEBIAN_FRONTEND=noninteractive
 apt update
 apt install libc6 -y
 apt install -y g++-11
-mkdir /home/user/123
-cd /home/user/123
-wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5-hotfix2/aleo_prover-v0.2.5_cuda_full_hotfix2.tar.gz
-tar -xzvf aleo_prover-v0.2.5_cuda_full_hotfix2.tar.gz
-wget https://github.com/apool-io/apoolminer/releases/download/v2.7.5/apoolminer_hiveos-v2.7.5.tar.gz
-tar -xzvf apoolminer_hiveos-v2.7.5.tar.gz
-wget https://raw.githubusercontent.com/forsbors/minescrs/main/qubic_aleo_zk_vrsc.sh
-mv qubic_aleo_zk_vrsc.sh qubic.sh
-chmod +x qubic.sh
-screen -dmS qubic /home/user/123/qubic.sh
